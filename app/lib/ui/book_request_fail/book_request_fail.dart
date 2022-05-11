5e93b869dd892846c235ab06fda6df4df6d74e40
victorpbp <victorrpeixotoo@gmail.com>
2022-05-11 00:15:49 -0300

    Adding Fail page to the Book Request

---
+++ app/lib/ui/book_request_fail/book_request_fail.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import '../../common/shelf_colors.dart';
+
+class BookRequestFail extends StatelessWidget {
+  const BookRequestFail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Card(
+        shadowColor: Colors.black,
+        margin: const EdgeInsets.symmetric(vertical: 180, horizontal: 250),
+        color: ShelfColors.yellowLight,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children:  [
+            const Center(
+              child: Icon(
+              Icons.cancel,
+              color: ShelfColors.pink,
+              size: 320,
+            ),),
+            const Center(
+              child: Text(
+                'Envio de solicitação falhou.',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 30),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(120, 60),
+                  primary: ShelfColors.pink,
+                  shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Voltar',
+                  style: TextStyle(color: Colors.black),),
+              ),
+            ),
+        ),],
+        ),
+      ),
+    );
+  }
+}
