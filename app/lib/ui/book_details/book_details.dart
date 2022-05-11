305ac01ea8808f465ee1e70ef0e2a4250bbdc37d
victorpbp <victorrpeixotoo@gmail.com>
2022-05-11 20:40:41 -0300

    Removing unecessary SizedBox

--- app/lib/ui/book_details/book_details.dart
+++ app/lib/ui/book_details/book_details.dart
@@ -9,106 +9,117 @@ class BookDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Card(
+      shadowColor: Colors.black,
+      margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 150),
+      color: ShelfColors.greenLight,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+      ),
 
-      //Card em si
-      child: Card(
-        shadowColor: Colors.black,
-        margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 150),
-        color: ShelfColors.greenLight,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(40.0)),
-        ),
-
-        //Definindo coluna com 2 elementos, imagens e conteúdos do livro e botão de doação.
-        child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.min,
-          children: [
-            //Definindo a linha que vai ter a imagem e a parte de conteúdos do livro
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.min,
-              children:  [
-                //Imagem do Livro
-                const Align(
+      //Definindo coluna com 2 elementos, imagens e conteúdos do livro e botão de doação.
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          //Definindo a linha que vai ter a imagem e a parte de conteúdos do livro
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //Imagem do Livro
+              const Align(
                 child: Padding(
-                padding: EdgeInsets.only(right: 100, left: 50),
-                child: Center(
-                  child: Text(
-                    'Aqui vem a imagem',
-                ),),),),
-                //Conteúdo Escrito do Livro
-                Column(      
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-
-                    Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                        'Nome do Livro',
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                    ),),),
-
-                    Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                        'Gênero do Livro',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                    ),),),
-
-                    Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                        'Estado: Novo ou Usado',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                    ),),),
-
-                    Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                        'Localização',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                    ),),),
-
-                    Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                        'Condição: Boa ou Mostra Marcas',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                    ),),),
-
-                    //Descrição mais elaborada do livro
-                    Padding(
-                        padding: EdgeInsets.all(16),
-                          child: Text(
-                          'Breve descrição pelo autor do anúncio sobre o livro.',
-                          style: TextStyle(
-                            //maxLines:3,
-                            overflow: TextOverflow.ellipsis,
-                            fontSize: 20,
-                    ),),),
-
-                  ],
+                  padding: EdgeInsets.only(right: 100, left: 50),
+                  child: Center(
+                    child: Text(
+                      'Aqui vem a imagem',
+                    ),
+                  ),
                 ),
-              ],       
-            ),
-            //Botão de Solicitação
-            Align(
+              ),
+              //Conteúdo Escrito do Livro
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Nome do Livro',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Gênero do Livro',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Estado: Novo ou Usado',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Localização',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Condição: Boa ou Mostra Marcas',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+                  //Descrição mais elaborada do livro
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Breve descrição pelo autor do anúncio sobre o livro.',
+                      maxLines: 3,
+                      style: TextStyle(
+                        //maxLines:3,
+                        overflow: TextOverflow.ellipsis,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          //Botão de Solicitação
+          Align(
             alignment: Alignment.centerRight,
             child: Padding(
               padding: const EdgeInsets.only(right: 60),
@@ -117,22 +128,22 @@ class BookDetails extends StatelessWidget {
                   minimumSize: const Size(260, 60),
                   primary: ShelfColors.green,
                   shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
                   ),
                 ),
-              onPressed: () {},
-              child: const Text(
-                'Solicitar Doação',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  ),),
+                onPressed: () {},
+                child: const Text(
+                  'Solicitar Doação',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- app/lib/ui/book_request_success/book_request_success.dart
+++ app/lib/ui/book_request_success/book_request_success.dart
@@ -7,54 +7,52 @@ class BookRequestSuccess extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Card(
-        shadowColor: Colors.black,
-        margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 250),
-        color: ShelfColors.yellowLight,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(40.0)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.min,
-          children:  [
-            const Center(
-              child: Icon(
-              Icons.check_circle,
-              color: ShelfColors.green,
-              size: 320,
-            ),),
-            const Center(
-              child: Text(
-                'Sua solicitação foi enviada!',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Card(
+      shadowColor: Colors.black,
+      margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 250),
+      color: ShelfColors.yellowLight,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children:  [
+          const Center(
+            child: Icon(
+            Icons.check_circle,
+            color: ShelfColors.green,
+            size: 320,
+          ),),
+          const Center(
+            child: Text(
+              'Sua solicitação foi enviada!',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 30),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(120, 60),
-                  primary: ShelfColors.green,
-                  shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                  ),
+          ),
+          Align(
+          alignment: Alignment.centerRight,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 30),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(120, 60),
+                primary: ShelfColors.green,
+                shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(32.0)),
                 ),
-                onPressed: () {},
-                child: const Text(
-                  'Concluir',
-                  style: TextStyle(color: Colors.black),),
               ),
+              onPressed: () {},
+              child: const Text(
+                'Concluir',
+                style: TextStyle(color: Colors.black),),
             ),
-        ),],
-        ),
+          ),
+      ),],
       ),
     );
   }
