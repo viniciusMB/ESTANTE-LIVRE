578065548a4cc73ec3362a100161ed6afb7de9a7
victorpbp <victorrpeixotoo@gmail.com>
2022-05-13 21:32:19 -0300

    Fixed positioning and width/height as well as made the button functional

--- app/lib/ui/book_details/book_details.dart
+++ app/lib/ui/book_details/book_details.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:estante_livre/ui/book_request_success/book_request_success.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/shelf_colors.dart';
@@ -9,141 +10,146 @@ class BookDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shadowColor: Colors.black,
-      margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 150),
-      color: ShelfColors.greenLight,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(40.0)),
-      ),
-
-      //Definindo coluna com 2 elementos, imagens e conteúdos do livro e botão de doação.
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          //Definindo a linha que vai ter a imagem e a parte de conteúdos do livro
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              //Imagem do Livro
-              const Align(
-                child: Padding(
-                  padding: EdgeInsets.only(right: 100, left: 50),
-                  child: Center(
-                    child: Text(
-                      'Aqui vem a imagem',
-                    ),
-                  ),
-                ),
-              ),
-              //Conteúdo Escrito do Livro
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Nome do Livro',
-                      style: TextStyle(
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Gênero do Livro',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Estado: Novo ou Usado',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Localização',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Condição: Boa ou Mostra Marcas',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-                  //Descrição mais elaborada do livro
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Breve descrição pelo autor do anúncio sobre o livro.',
-                      maxLines: 3,
-                      style: TextStyle(
-                        //maxLines:3,
-                        overflow: TextOverflow.ellipsis,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        height: (MediaQuery.of(context).size.height * 0.8),
+        child: Card(
+          shadowColor: Colors.black,
+          color: ShelfColors.greenLight,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(40.0)),
           ),
-          //Botão de Solicitação
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 60),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(260, 60),
-                  primary: ShelfColors.green,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+
+          //Definindo a coluna que vai ter a imagem e a parte de conteúdos do livro
+
+          child: Padding(
+            padding: const EdgeInsets.all(48),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center, // Vertical -- Row
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceEvenly, //Horizontal -- Row
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //Imagem do Livro
+                Align(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 46,
+                    ),
+                    child: SizedBox(
+                      height: 450,
+                      width: 275,
+                      child: Image.asset(
+                        'assets/images/test_BD.jpg',
+                      ),
+                    ),
                   ),
                 ),
-                onPressed: () {},
-                child: const Text(
-                  'Solicitar Doação',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                //Conteúdo Escrito do Livro
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Nome do Livro',
+                        style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 48), //Espaçamento
+                      const Text(
+                        'Gênero do Livro',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 24), //Espaçamento
+                      const Text(
+                        'Estado: Novo ou Usado',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 24), //Espaçamento
+                      const Text(
+                        'Localização',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 24), //Espaçamento
+                      const Text(
+                        'Condição: Boa ou Mostra Marcas',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 24), //Espaçamento
+                      //Descrição mais elaborada do livro
+                      const Text(
+                        'Breve descrição pelo autor do anúncio sobre o livro.',
+                        maxLines: 3,
+                        style: TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(height: 24), //Espaçamento
+                      //Botão de Solicitação
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 30), //right:10
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(
+                                260,
+                                60,
+                              ),
+                              primary: ShelfColors.green,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(32.0),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return const Dialog(
+                                    backgroundColor: Colors.transparent,
+                                    child: BookRequestSuccess(),
+                                  );
+                                },
+                              );
+                            },
+                            child: const Text(
+                              'Solicitar Doação',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
