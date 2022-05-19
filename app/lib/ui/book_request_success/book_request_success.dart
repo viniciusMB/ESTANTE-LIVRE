d580a8cf79f7485101c3983ccc640a8ee1b246c9
victorpbp <victorrpeixotoo@gmail.com>
2022-05-19 00:13:10 -0300

    Fixed Widgets constraints and width/height

--- app/lib/ui/book_request_success/book_request_success.dart
+++ app/lib/ui/book_request_success/book_request_success.dart
@@ -8,62 +8,69 @@ class BookRequestSuccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Card em si
-    return Card(
-      shadowColor: Colors.black,
-      margin: const EdgeInsets.symmetric(vertical: 80, horizontal: 250),
-      color: ShelfColors.yellowLight,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(40.0)),
-      ),
-
-      //Coluna contendo o ícone, descrição e botão
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          //Ícone
-          const Center(
-            child: Icon(
-              Icons.check_circle,
-              color: ShelfColors.green,
-              size: 320,
-            ),
+    return Center(
+      child: SizedBox(
+        //margin: const EdgeInsets.symmetric(horizontal: 24),
+        height: (MediaQuery.of(context).size.height * 0.7),
+        width: (MediaQuery.of(context).size.height * 0.7),
+        child: Card(
+          shadowColor: Colors.black,
+          //margin: const EdgeInsets.symmetric(vertical: 80, horizontal: 250), //Need to make a sizedBox to Wrap the Card and pass the width and height there.
+          color: ShelfColors.yellowLight,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(40.0)),
           ),
 
-          //Descrição
-          const Center(
-            child: Text(
-              'Sua solicitação foi enviada!',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
+          //Coluna contendo o ícone, descrição e botão
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //Ícone
+              const Center(
+                child: Icon(
+                  Icons.check_circle,
+                  color: ShelfColors.green,
+                  size: 320,
+                ),
               ),
-            ),
-          ),
 
-          //Botão do tipo Elevated Button
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 30, bottom: 20),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(120, 60),
-                  primary: ShelfColors.green,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //Descrição
+              const Center(
+                child: Text(
+                  'Sua solicitação foi enviada!',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text(
-                  'Concluir',
-                  style: TextStyle(color: Colors.black),
+              ),
+
+              //Botão do tipo Elevated Button
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 30, bottom: 20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(120, 60),
+                      primary: ShelfColors.green,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                      ),
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text(
+                      'Concluir',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
