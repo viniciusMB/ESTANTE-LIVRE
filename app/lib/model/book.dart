c4a04392832c4f5f9dba4254a2cc320155506925
Edson Gonzalez <gonzalezedson.junior@gmail.com>
2022-05-23 23:54:33 -0300

    feat: WIP - catalog screen

---
+++ app/lib/model/book.dart
@@ -0,0 +1,11 @@
+class Book {
+  final String title;
+  final String location;
+  final String bookCondition;
+
+  const Book({
+    required this.title,
+    required this.location,
+    required this.bookCondition,
+  });
+}
--- app/lib/ui/catalog/catalog.dart
+++ app/lib/ui/catalog/catalog.dart
@@ -1,41 +1,200 @@
+import 'dart:math';
+
+import 'package:estante_livre/model/book.dart';
 import 'package:flutter/material.dart';
 
-class Catalog extends StatelessWidget {
+import '../../common/shelf_colors.dart';
+
+class Catalog extends StatefulWidget {
   const Catalog({Key? key}) : super(key: key);
 
   @override
+  State<Catalog> createState() => _CatalogState();
+}
+
+class _CatalogState extends State<Catalog> {
+  static const List<String> _locations = [
+    'Salvador',
+    'Vitória da Conquista',
+    'Camaçari',
+  ];
+
+  static const List<String> _bookConditions = [
+    'Usado',
+    'Novo',
+  ];
+
+  static const List<Book> _mockBookData = [
+    Book(
+      title: 'Percy Jackson: O ladrão de raios',
+      location: 'Salvador',
+      bookCondition: 'Usado',
+    ),
+    Book(
+      title: 'Harry Potter e a pedra filosofal',
+      location: 'Vitória da Conquista',
+      bookCondition: 'Novo',
+    ),
+    Book(
+      title: 'O livro de ponteiros',
+      location: 'Camaçari',
+      bookCondition: 'Usado',
+    ),
+    Book(
+      title: 'A culpa é das estrelas',
+      location: 'Camaçari',
+      bookCondition: 'Novo',
+    ),
+    Book(
+      title: '365 dias',
+      location: 'Vitória da Conquista',
+      bookCondition: 'Usado',
+    ),
+    Book(
+      title: 'O Senhor dos Anéis',
+      location: 'Salvador',
+      bookCondition: 'Novo',
+    ),
+  ];
+
+  String _currentLocation = '';
+  String _currentBookCondition = '';
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          margin: const EdgeInsets.only(
-            top: 33,
-            bottom: 50,
-          ),
-          padding: const EdgeInsets.symmetric(
-            vertical: 5,
-            horizontal: 10,
-          ),
-          width: MediaQuery.of(context).size.width,
-          color: Theme.of(context).colorScheme.surfaceVariant,
-          child: Text(
-            'Navegando por',
-            style: Theme.of(context).textTheme.headline1,
-          ),
-        ),
-        Row(
-          children: [
-            Container(
-              color: Theme.of(context).colorScheme.surface,
-              child: const Text('Catálogo'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 33,
+              bottom: 50,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Novo'),
+            padding: const EdgeInsets.symmetric(
+              vertical: 5,
+              horizontal: 10,
             ),
-          ],
-        ),
-      ],
+            width: MediaQuery.of(context).size.width,
+            color: ShelfColors.blueLighter,
+            child: Text(
+              'Navegando por',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: ShelfColors.greenLight,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 28,
+                  horizontal: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Filtros',
+                      style: Theme.of(context).textTheme.headline3?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 10, top: 30),
+                      child: Text('Localização'),
+                    ),
+                    ..._locations
+                        .map(
+                          (location) => Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Row(children: [
+                              Checkbox(
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(2)),
+                                ),
+                                value: location == _currentLocation,
+                                onChanged: (bool? selected) => setState(() {
+                                  _currentLocation =
+                                      selected ?? false ? location : '';
+                                }),
+                              ),
+                              Text(location),
+                            ]),
+                          ),
+                        )
+                        .toList(),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 10, top: 20),
+                      child: Text('Estado'),
+                    ),
+                    ..._bookConditions
+                        .map(
+                          (condition) => Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Row(children: [
+                              Checkbox(
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(2)),
+                                ),
+                                value: condition == _currentBookCondition,
+                                onChanged: (bool? selected) => setState(() {
+                                  _currentBookCondition =
+                                      selected ?? false ? condition : '';
+                                }),
+                              ),
+                              Text(condition),
+                            ]),
+                          ),
+                        )
+                        .toList(),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              SizedBox(
+                height: 700,
+                width: 1010,
+                child: GridView(
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    childAspectRatio: 3 / 2,
+                  ),
+                  children: _mockBookData
+                      .where((element) {
+                        bool locationMatch = _currentLocation == '' ||
+                            element.location == _currentLocation;
+
+                        bool conditionMatch = _currentBookCondition == '' ||
+                            element.bookCondition == _currentBookCondition;
+
+                        return locationMatch && conditionMatch;
+                      })
+                      .map((book) => Container(
+                            color: ShelfColors.greenLight,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(book.title),
+                                Text(book.location),
+                                Text(book.bookCondition),
+                              ],
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
