8727ce5d607fb007ddc14a1c0c4b90504796d4cf
Maria Luisa <araujocostamalu@gmail.com>
2022-05-29 10:18:48 -0300

    refactor: improves the structure of my_area

--- app/lib/ui/my_area/my_area.dart
+++ app/lib/ui/my_area/my_area.dart
@@ -1,6 +1,7 @@
-import 'package:estante_livre/common/shelf_colors.dart';
+//import 'package:estante_livre/common/shelf_colors.dart';
 import 'package:estante_livre/ui/my_area/widgets/book_tile.dart';
-import 'package:estante_livre/ui/my_area/widgets/space_widget.dart';
+import 'package:estante_livre/ui/my_area/widgets/button.dart';
+import 'package:estante_livre/ui/my_area/widgets/subtitle.dart';
 import 'package:flutter/material.dart';
 
 class MyArea extends StatelessWidget {
@@ -8,115 +9,66 @@ class MyArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
-            top: 50,
-            right: 0,
-            bottom: 20,
+    const numberOfBooks = 2;
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          //Header my_area
+          Row(
+            children: const [
+              Expanded(
+                child: Text(
+                  'Olá, nomezin da pessoa!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+              ),
+              Button(
+                text: 'Adicionar anúncio',
+                height: 56,
+                width: 280,
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              Button(
+                text: 'Sair',
+                height: 56,
+                width: 126,
+                isAlert: true,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
           ),
-          child: SizedBox(
-            child: SizedBox(
-              width: 1290,
-              height: 56,
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      'Olá! Nomezin da pessoa',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 36,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 280,
-                    height: 56,
-                    child: TextButton(
-                      child: const Text(
-                        'Adicionar anúncio',
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: ShelfColors.greenLight,
-                        onPrimary: const Color.fromARGB(255, 64, 64, 64),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 50),
-                  SizedBox(
-                    width: 126,
-                    height: 56,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Sair',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: ShelfColors.pink,
-                        onPrimary: const Color.fromARGB(255, 64, 64, 64),
-                      ),
-                    ),
-                  ),
-                ],
+          const SizedBox(
+            height: 44,
+          ),
+          const Subtitle(text: 'Seus anúncios: '),
+          const SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: numberOfBooks,
+              itemBuilder: (BuildContext context, int index) {
+                return const BookTile();
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const SizedBox(
+                height: 50,
               ),
             ),
           ),
-        ),
-        Container(
-          color: ShelfColors.blueLight,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 40,
-              top: 10,
-              right: 40,
-              bottom: 10,
-            ),
-            child: Row(
-              children: const [
-                Expanded(
-                  flex: 10,
-                  child: Text(
-                    'Seus anúncios: ',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      backgroundColor: ShelfColors.blueLight,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        const SpaceWidget(),
-        Expanded(
-          child: ListView.separated(
-            padding: const EdgeInsets.all(8),
-            itemCount: 2,
-            itemBuilder: (BuildContext context, int index) {
-              return const BookTile();
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const SpaceWidget(),
-          ),
-        ),
-        const SpaceWidget(),
-        const SpaceWidget(),
-        const SpaceWidget(),
-      ],
+        ],
+      ),
     );
   }
 }
---
+++ app/lib/ui/my_area/widgets/button.dart
@@ -0,0 +1,45 @@
+import 'package:estante_livre/common/shelf_colors.dart';
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final double height;
+  final double width;
+  final bool isAlert;
+
+  const Button({
+    Key? key,
+    required this.text,
+    required this.height,
+    required this.width,
+    this.isAlert = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color buttonColor;
+    buttonColor = isAlert == true ? ShelfColors.red : ShelfColors.green;
+
+    return InkWell(
+      onTap: () {},
+      customBorder: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Container(
+        height: height,
+        width: width,
+        child: Text(text),
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        decoration: ShapeDecoration(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          color: buttonColor,
+        ),
+      ),
+    );
+  }
+}
